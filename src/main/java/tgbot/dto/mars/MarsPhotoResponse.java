5a188547315f927682f7bf56dd07a3023cbeda22
renatgh <renatgh@gmail.com>
2023-07-08 00:24:21 +0300

    logs6

--- src/main/java/tgbot/dto/mars/MarsPhotoResponse.java
+++ src/main/java/tgbot/dto/mars/MarsPhotoResponse.java
@@ -1,8 +1,11 @@
 package tgbot.dto.mars;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class MarsPhotoResponse {
     @JsonProperty("photos")
     private final List<MarsPhoto> photos;
